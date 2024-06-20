db54adc875a5420e3238d9745e8b4e4434820adc
Martin <martinbruun1995@gmail.com>
2024-06-20 13:54:17 +0200

    feat: Update device types and add missing properties

---
+++ types/device.ts
@@ -0,0 +1,36 @@
+export interface Root {
+  errors: any[];
+  data: Daum[];
+}
+
+export interface Daum {
+  id: string;
+  id_v1: string;
+  product_data: ProductData;
+  metadata: Metadata;
+  identify: Identify;
+  services: Service[];
+  type: string;
+}
+
+export interface ProductData {
+  model_id: string;
+  manufacturer_name: string;
+  product_name: string;
+  product_archetype: string;
+  certified: boolean;
+  software_version: string;
+  hardware_platform_type: string;
+}
+
+export interface Metadata {
+  name: string;
+  archetype: string;
+}
+
+export interface Identify {}
+
+export interface Service {
+  rid: string;
+  rtype: string;
+}
