6e27ffd260e78e71e7405ca71d2c337d96e974a5
Martin <martinbruun1995@gmail.com>
2024-06-27 20:45:26 +0200

    refactor: Remove unused ModalScreen component

--- app/modal.tsx
+++
@@ -1,5 +0,0 @@
-import { Anchor, Paragraph, View, XStack } from 'tamagui';
-
-export default function ModalScreen() {
-  return <View flex={1} alignItems="center" justifyContent="center"></View>;
-}
