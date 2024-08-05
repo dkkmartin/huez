daa60ea06ad9a29b5ea6e86dd228baab0960a4fd
Martin <martinbruun1995@gmail.com>
2024-08-05 10:21:15 +0200

    refactor: Update settings page to include test connection button

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,17 +1,25 @@
-import { Button, YStack } from 'tamagui';
-import { Appearance } from 'react-native';
-import { Moon, Sun } from '@tamagui/lucide-icons';
-import { HueBridgeSVG } from 'components/svg/hueBridgeSVG';
-import { useEffect } from 'react';
-import { LogBox } from 'react-native';
-import { Link } from 'expo-router';
+import { Button, YStack } from 'tamagui'
+import { Appearance } from 'react-native'
+import { Moon, Sun } from '@tamagui/lucide-icons'
+import { HueBridgeSVG } from 'components/svg/hueBridgeSVG'
+import { LogBox } from 'react-native'
+import { Link } from 'expo-router'
+import { getData } from 'lib/asyncStorage'
+import { getDevices } from 'lib/hue'
 
 export default function TabSettings() {
-  const colorScheme = Appearance.getColorScheme();
-  LogBox.ignoreLogs([/bad setState[\s\S]*Themed/]);
+  const colorScheme = Appearance.getColorScheme()
+  LogBox.ignoreLogs([/bad setState[\s\S]*Themed/])
 
   function handleThemeSwitch() {
-    Appearance.setColorScheme(colorScheme === 'dark' ? 'light' : 'dark');
+    Appearance.setColorScheme(colorScheme === 'dark' ? 'light' : 'dark')
+  }
+
+  async function testConnection() {
+    const bridgeIP = await getData('bridge-ip')
+    const bridgeKey = await getData('bridge-key')
+    console.log(bridgeIP, bridgeKey)
+    getDevices(bridgeIP as string, bridgeKey as string)
   }
 
   return (
@@ -21,7 +29,9 @@ export default function TabSettings() {
         size="$5"
         padding="$0"
         variant="outlined"
-        icon={colorScheme === 'dark' ? <Moon size={'$3'} /> : <Sun size={'$3'} />}
+        icon={
+          colorScheme === 'dark' ? <Moon size={'$3'} /> : <Sun size={'$3'} />
+        }
       >
         {colorScheme === 'dark' ? 'Dark' : 'Light'}
       </Button>
@@ -30,6 +40,9 @@ export default function TabSettings() {
           Connect bridge
         </Button>
       </Link>
+      <Button onPress={testConnection} size={'$5'} variant="outlined">
+        Test
+      </Button>
     </YStack>
-  );
+  )
 }
--- lib/hue.ts
+++ lib/hue.ts
@@ -2,24 +2,44 @@ export async function getHueKey(IP: string) {
   let headersList = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-  };
+  }
 
   let bodyContent = JSON.stringify({
     devicetype: 'app_name#instance_name',
     generateclientkey: true,
-  });
+  })
 
   try {
     const response = await fetch(`http://${IP}/api`, {
       method: 'POST',
       body: bodyContent,
       headers: headersList,
-    });
+    })
 
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.log(error);
-    return error;
+    console.log(error)
+    return error
+  }
+}
+
+export async function getDevices(IP: string, key: string) {
+  let headersList = {
+    'hue-application-key': key,
+    'Content-Type': 'application/json',
+  }
+
+  try {
+    const response = await fetch(`https://${IP}/clip/v2/resource/device`, {
+      method: 'GET',
+      headers: headersList,
+    })
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+    return error
   }
 }
