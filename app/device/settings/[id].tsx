4f9de4b60baa49727d0dd8af65f522316e354ce8
Martin <martinbruun1995@gmail.com>
2024-08-09 12:51:17 +0200

    feat: Add color picker to device settings page

---
+++ app/device/settings/[id].tsx
@@ -0,0 +1,52 @@
+import { useLocalSearchParams } from 'expo-router'
+import React, { useState } from 'react'
+import { Button, Modal, StyleSheet, View } from 'react-native'
+
+import ColorPicker, {
+  Panel1,
+  Swatches,
+  Preview,
+  OpacitySlider,
+  HueSlider,
+} from 'reanimated-color-picker'
+
+export default function DeviceSettings() {
+  const { id } = useLocalSearchParams()
+  const [showModal, setShowModal] = useState(false)
+
+  // Note: 👇 This can be a `worklet` function.
+  const onSelectColor = ({ hex }) => {
+    // do something with the selected color.
+    console.log(hex)
+  }
+
+  return (
+    <View style={styles.container}>
+      <ColorPicker />
+      <Button title="Color Picker" onPress={() => setShowModal(true)} />
+
+      <Modal visible={showModal} animationType="slide">
+        <ColorPicker
+          style={{ width: '70%' }}
+          value="red"
+          onComplete={onSelectColor}
+        >
+          <Preview />
+          <Panel1 />
+          <HueSlider />
+          <OpacitySlider />
+          <Swatches />
+        </ColorPicker>
+
+        <Button title="Ok" onPress={() => setShowModal(false)} />
+      </Modal>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+})
