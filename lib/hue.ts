13cfcea8ac9124a1575664a00411f0f75bb04b11
Martin <martinbruun1995@gmail.com>
2024-08-13 12:28:51 +0200

    refactor: Update getDevices function to include fetching device information by ID

--- lib/hue.ts
+++ lib/hue.ts
@@ -25,14 +25,57 @@ export async function getHueKey(IP: string) {
 }
 
 export async function getDevices(IP: string, key: string) {
-  let bodyContent = JSON.stringify({
-    key: key,
-  })
-
   try {
     const response = await fetch(`http://localhost:3000/${IP}/${key}/devices`, {
       method: 'POST',
     })
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+}
+
+export async function getDeviceInfo(IP: string, key: string, ID: string) {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/${IP}/${key}/${ID}/device`
+    )
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+}
+
+export async function changeColor(
+  IP: string,
+  key: string,
+  ID: string,
+  hex: string
+) {
+  let bodyContent = JSON.stringify({
+    color: hex,
+  })
+
+  let headersList = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  }
+
+  try {
+    const response = await fetch(
+      `http://localhost:3000/${IP}/${key}/${ID}/color`,
+      {
+        method: 'POST',
+        body: bodyContent,
+        headers: headersList,
+      }
+    )
 
     const data = await response.json()
     return data
