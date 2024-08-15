e777246d776c0269c2a1165f5905baec4b92cacd
Martin <martinbruun1995@gmail.com>
2024-08-15 13:54:28 +0200

    refactor: Update GroupCard component to use device name instead of archetype for icon selection

--- components/group/groupCard.tsx
+++ components/group/groupCard.tsx
@@ -1,73 +1,57 @@
+import { useEffect, useState } from 'react'
+import { JSX } from 'react/jsx-runtime'
+import { Button, H2, XStack, Card, Slider } from 'tamagui'
 import {
-  SlidersHorizontal,
-  Lamp,
-  Sun,
-  SunDim,
   ChefHat,
   Sofa,
-  Bath,
-  Car,
-  Briefcase,
-  LampDesk,
   LampCeiling,
-} from '@tamagui/lucide-icons';
-import { useEffect, useState } from 'react';
-import { JSX } from 'react/jsx-runtime';
-import { Button, H2, XStack, Card, Slider } from 'tamagui';
+  SlidersHorizontal,
+} from '@tamagui/lucide-icons'
+import LightSliderGroup from 'components/device/lightSliderGroup'
+
+interface Device {
+  id: string
+  name: string
+  room: string
+  lightRid: string
+}
 
 interface Group {
-  type: string;
-  id: string;
-  children: Array<{
-    rid: string;
-    rtype: string;
-  }>;
-  metadata: {
-    name: string;
-    archetype: string;
-  };
+  name: string
+  devices: Device[]
 }
 
 export default function GroupCard({
   groupData,
   setCanScroll,
 }: {
-  groupData: Group;
-  setCanScroll: (value: boolean) => void;
+  groupData: Group
+  setCanScroll: (value: boolean) => void
 }) {
-  const [icon, setIcon] = useState<JSX.Element | null>(null);
+  const [icon, setIcon] = useState<JSX.Element | null>(null)
 
-  let newIcon: JSX.Element;
+  let newIcon: JSX.Element
   useEffect(() => {
-    switch (groupData.metadata.archetype) {
+    switch (groupData.name) {
       case 'kitchen':
-        newIcon = <ChefHat size={80} />;
-        break;
-      case 'living_room':
-        newIcon = <Sofa size={80} />;
-        break;
-      case 'bathroom':
-        newIcon = <Bath size={80} />;
-        break;
-      case 'garage':
-        newIcon = <Car size={80} />;
-        break;
-      case 'office':
-        newIcon = <Briefcase size={80} />;
-        break;
+        newIcon = <ChefHat size={80} />
+        break
+      case 'living room':
+        newIcon = <Sofa size={80} />
+        break
       default:
-        newIcon = <LampCeiling size={80} />;
-        break;
+        newIcon = <LampCeiling size={80} />
+        break
     }
-    setIcon(newIcon);
-  }, [groupData.metadata.archetype]);
+    setIcon(newIcon)
+  }, [groupData.name])
 
   return (
     <Card bordered elevate width={360}>
       <Card.Header>
         <XStack justifyContent="space-between">
           <H2 size="$8" style={{ fontWeight: 'bold' }}>
-            {groupData.metadata.name}
+            {groupData.name}
           </H2>
           <Button>
             <SlidersHorizontal />
@@ -77,26 +61,8 @@ export default function GroupCard({
 
       <XStack justifyContent="center">{icon}</XStack>
       <Card.Footer padded>
-        <XStack alignItems="center" margin="auto" gap={8}>
-          <SunDim></SunDim>
-          <Slider
-            onSlideMove={() => setCanScroll(false)}
-            onSlideEnd={() => setCanScroll(true)}
-            size="$2"
-            width={250}
-            defaultValue={[50]}
-            max={100}
-            step={1}
-          >
-            <Slider.Track>
-              <Slider.TrackActive />
-            </Slider.Track>
-            <Slider.Thumb circular index={0} />
-          </Slider>
-
-          <Sun></Sun>
-        </XStack>
+        <LightSliderGroup />
       </Card.Footer>
     </Card>
-  );
+  )
 }
