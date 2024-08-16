a8b9ba52268cda15adc366f425519b052af7fea3
Martin <martinbruun1995@gmail.com>
2024-08-16 12:08:06 +0200

    refactor: Update LightSliderGroup component to use new grouped_light type and changeRoomBrightness function

--- components/device/lightSliderGroup.tsx
+++ components/device/lightSliderGroup.tsx
@@ -1,39 +1,46 @@
 import { Sun, SunDim } from '@tamagui/lucide-icons'
 import { useFocusEffect } from 'expo-router'
 import { getData } from 'lib/asyncStorage'
-import { changeBrightness, getBrightness } from 'lib/hue'
+import { changeRoomBrightness, getGroupedLight } from 'lib/hue'
 import { useCallback, useEffect, useState } from 'react'
 import { Slider, XStack } from 'tamagui'
-import { Root } from 'types/deviceLightInfo'
+import { Root } from 'types/grouped_light'
 
 export default function LightSliderGroup({
   IP,
   ID,
-  RID,
 }: {
   IP: string
   ID: string
-  RID: string
 }) {
   const [bridgeKey, setBridgeKey] = useState('')
-  const [deviceInfo, setDeviceInfo] = useState<Root>()
+  const [groupLight, setGroupLight] = useState<Root>()
 
   function brightness(value: any) {
     value = Math.floor(value)
     if (value > 100) value = 100
     if (value < 0) value = 0
-    changeBrightness(IP, bridgeKey, RID, value)
+    changeRoomBrightness(IP, bridgeKey, ID, value)
   }
 
   useFocusEffect(
     useCallback(() => {
-      setDeviceInfo(undefined)
+      setGroupLight(undefined)
       if (!bridgeKey || !ID) return
-      async function fetchDeviceInfo() {
-        const response = await getBrightness(IP, bridgeKey, RID)
-        setDeviceInfo(response)
+
+      // if going into settings and then go back the first fetch for brightness info is gonna fail...
+      // Recursive calling itself until its working with a max of 10 tries
+      async function fetchDeviceInfoWithRetry(attempt = 1) {
+        const response = await getGroupedLight(IP, bridgeKey, ID)
+        if (response.data === undefined && attempt < 10) {
+          setTimeout(() => fetchDeviceInfoWithRetry(attempt + 1), 1000)
+        } else {
+          if (attempt >= 10) return
+          setGroupLight(response)
+        }
       }
-      fetchDeviceInfo()
+
+      fetchDeviceInfoWithRetry()
 
       return () => {}
     }, [bridgeKey, ID])
@@ -47,7 +54,7 @@ export default function LightSliderGroup({
     getBridgeKey()
   }, [])
 
-  if (deviceInfo)
+  if (groupLight?.data[0])
     return (
       <XStack alignItems="center" gap={8} width="100%" justifyContent="center">
         <SunDim></SunDim>
@@ -56,7 +63,7 @@ export default function LightSliderGroup({
           maxWidth={300}
           onSlideEnd={(event, value) => brightness(value)}
           size="$2"
-          defaultValue={[deviceInfo?.data[0].dimming.brightness as number]}
+          defaultValue={[groupLight?.data[0].dimming.brightness as number]}
           max={100}
           step={1}
         >
