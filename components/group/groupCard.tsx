fed2503b8f96da9f12283bdaf8c8f7763ad409d0
Martin <martinbruun1995@gmail.com>
2024-08-16 12:08:11 +0200

    refactor: Update GroupCard component to use device name instead of archetype for icon selection

--- components/group/groupCard.tsx
+++ components/group/groupCard.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react'
 import { JSX } from 'react/jsx-runtime'
-import { Button, H2, XStack, Card, Slider } from 'tamagui'
+import { Button, H2, XStack, Card } from 'tamagui'
 import {
   ChefHat,
   Sofa,
   LampCeiling,
   SlidersHorizontal,
 } from '@tamagui/lucide-icons'
+import { Daum } from 'types/rooms'
+import { Link } from 'expo-router'
 import LightSliderGroup from 'components/device/lightSliderGroup'
+import { getData } from 'lib/asyncStorage'
+import { Daum as groupedDaum } from 'types/grouped_light'
+import { getGroupedLights } from 'lib/hue'
 
 interface Device {
   id: string
@@ -16,23 +21,43 @@ interface Device {
   lightRid: string
 }
 
-interface Group {
-  name: string
-  devices: Device[]
-}
-
 export default function GroupCard({
   groupData,
   setCanScroll,
 }: {
-  groupData: Group
+  groupData: Daum
   setCanScroll: (value: boolean) => void
 }) {
   const [icon, setIcon] = useState<JSX.Element | null>(null)
+  const [bridgeIP, setBridgeIP] = useState('')
+  const [bridgeKey, setBridgeKey] = useState('')
+  const [FilteredGroupLight, setFilteredGroupLight] = useState<groupedDaum>()
+
+  useEffect(() => {
+    async function getStorage() {
+      const bridgeIP = await getData('bridge-ip')
+      const bridgeKey = await getData('bridge-key')
+      setBridgeIP(bridgeIP as string)
+      setBridgeKey(bridgeKey as string)
+    }
+    getStorage()
+  }, [])
+
+  useEffect(() => {
+    if (!bridgeIP && !bridgeKey) return
+    async function x() {
+      const groupedLightResponse = await getGroupedLights(bridgeIP, bridgeKey)
+      const filteredGroup = groupedLightResponse.data.find(
+        (light) => light.owner.rid === groupData.id
+      )
+      setFilteredGroupLight(filteredGroup)
+    }
+    x()
+  }, [groupData])
 
   let newIcon: JSX.Element
   useEffect(() => {
-    switch (groupData.name) {
+    switch (groupData.metadata.archetype) {
       case 'kitchen':
         newIcon = <ChefHat size={80} />
         break
@@ -44,24 +69,32 @@ export default function GroupCard({
         break
     }
     setIcon(newIcon)
-  }, [groupData.name])
+  }, [groupData.metadata.name])
 
   return (
     <Card bordered elevate width={360}>
       <Card.Header>
         <XStack justifyContent="space-between">
           <H2 size="$8" style={{ fontWeight: 'bold' }}>
-            {groupData.name}
+            {groupData.metadata.name}
           </H2>
-          <Button>
-            <SlidersHorizontal />
-          </Button>
+          <Link
+            href={{
+              //@ts-ignore
+              pathname: '/room/settings/[id]',
+              params: { id: groupData.id },
+            }}
+          >
+            <Button>
+              <SlidersHorizontal />
+            </Button>
+          </Link>
         </XStack>
       </Card.Header>
 
       <XStack justifyContent="center">{icon}</XStack>
       <Card.Footer padded>
-        <LightSliderGroup />
+        <LightSliderGroup ID={FilteredGroupLight?.id as string} IP={bridgeIP} />
       </Card.Footer>
     </Card>
   )
